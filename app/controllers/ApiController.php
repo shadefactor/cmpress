8fc42640aa30ec6290474fe3e487e1f5715fb14c
buonzz <buonzz@gmail.com>
2014-09-14 20:57:07 +0800

    refactored api hooks

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,27 +1,25 @@
 <?php
 
 class ApiController extends BaseController {
-	public function receive(){
-		
-		\Podio::setup('podiopress-demo', 'DAbnfrnGqE6GLtMAvIoToj9gPCzXJ54UJdNY8l16M3pH9h9pBG65KK9MRTwWj64y');
-		\Podio::authenticate_with_app('9367689', '0ad62c85647c41229b9dba0bd786e868');
+	public function receive(){		
 
 		switch ($_POST['type']) {
 		  case 'hook.verify':
-		    // Validate the webhook
+		    \Podio::setup('podiopress-demo', 'DAbnfrnGqE6GLtMAvIoToj9gPCzXJ54UJdNY8l16M3pH9h9pBG65KK9MRTwWj64y');
+			\Podio::authenticate_with_app('9367689', '0ad62c85647c41229b9dba0bd786e868');
 		    \PodioHook::validate($_POST['hook_id'], array('code' => $_POST['code']));
 		  case 'item.create':
-		    // Do something. item_id is available in $_POST['item_id']
+		    $pb = new Postback;
+			$pb->data = serialize($_POST);
+			$pb->pb_type = $_POST['type'];
+			$pb->item_id = $_POST['item_id'];
+			$pb->save();		
 		  case 'item.update':
 		    // Do something. item_id is available in $_POST['item_id']
 		  case 'item.delete':
 		    // Do something. item_id is available in $_POST['item_id']
 		}
 
-		$pb = new Postback;
-		$pb->data = serialize($_POST);
-		$pb->pb_type = $_POST['type'];
-		$pb->save();		
 		return 'hey api received';
 	}
 	public function test(){
